2f44ca2dd48aff4f0382f18842c3e55aeb5c1a09
quentin laval <laval.quentin80@gmail.com>
2023-11-15 09:10:16 +0100

    commit1

---
+++ exo2.js
@@ -0,0 +1,10 @@
+// chaine de caractère 
+var MavariableString ="toto"
+//nombre
+var MavariableNumérique= 100;
+// des tableaux
+var MavariableTableau = ['Apple', 'Banana'];
+//des objet (ici la fenetre du navigateur)
+var MavariableObjet = window
+
+alert(MavariableString + MavariableNumérique + MavariableTableau[0] + MavariableObjet.location.pathname;)
